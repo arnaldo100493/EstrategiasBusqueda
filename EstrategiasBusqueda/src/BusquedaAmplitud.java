06de77db08d2c654d5d5675b8a49c6b214478cbc
arnaldo100493 <cyberarnaldo04@hotmail.com>
2017-06-05 14:47:50 -0500

    El proyecto se ha generado correctamente.

--- EstrategiasBusqueda/src/BusquedaAmplitud.java
+++ EstrategiasBusqueda/src/BusquedaAmplitud.java
@@ -16,7 +16,6 @@ public class BusquedaAmplitud {
     private ArrayList caminos;
     private ArrayList colaNodos;
     
-
     public BusquedaAmplitud() {
         this.visitados = new ArrayList();
         this.caminos = new ArrayList();
